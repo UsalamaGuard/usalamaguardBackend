71cdfb0904a48970ae332074d12c0da4215846bc
AtMwach <mwachalanyambu@gmail.com>
2025-04-03 14:45:55 +0300

    Defined dotenv

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const router = express.Router();
+require("dotenv").config();
 
 // Register
 router.post("/register", async (req, res) => {
